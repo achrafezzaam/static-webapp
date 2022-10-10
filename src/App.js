7d2927f5acbb3d176717d30916cec185f7b40f43
achrafezzaam <achraf.ezzaam@gmail.com>
2022-10-10 16:02:46 +0100

    feat: creating the files and writing the first components
    Installing the project dependecies and writting the first
    three components: Header, App and Footer. Including them
    to the App.js file. Next step should be including the
    styling to the app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={logo} width="100px" alt="App logo" />
+      </nav>
+    </header>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be 
+        able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer
+        if I know React</li>
+      </ol>
     </div>
   );
 }
 
-export default App;
+function Footer() {
+  return(
+    <footer>
+      <small>© 2022 Achraf Ezzaam development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+export {Header, App, Footer};
